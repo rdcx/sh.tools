214c8c10bf4de67c92eb063bcfb2d5cba192fadc
Ross Chater <rdchater@gmail.com>
2023-11-08 20:25:17 +0000

    speedtest

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/linux/speedtest", func(c *gin.Context) {
+		d, err := os.ReadFile("tools/linux/speedtest/speedtest-go")
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		c.Data(http.StatusOK, "application/octet-stream", d)
+	})
+
+	r.GET("/linux/speedtest.sh", func(c *gin.Context) {
+		d, err := os.ReadFile("tools/linux/speedtest/speedtest.sh")
+
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		c.Data(http.StatusOK, "text/plain", d)
+	})
+
+	r.Run(":8080")
+}
---
+++ tools/linux/speedtest/speedtest.sh
@@ -0,0 +1,15 @@
+# download speedtest from sh.tools/linux/speedtest
+# and run it
+
+# download speedtest
+wget -q -O speedtest http://sh.tools/linux/speedtest
+
+# make speedtest executable
+chmod +x speedtest
+
+# run speedtest
+./speedtest
+
+# remove speedtest
+rm speedtest
+
